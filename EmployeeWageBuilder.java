8bb8d10f3a9cb41f32578fc23595a2dfe5d40bb5
AkshataHasabi <akshuh818@gmail.com>
2022-01-03 20:40:07 +0530

    ADDED:checked if employee is present or absent

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,9 +1,20 @@
 package com.bridgelab.employeewage;
 
 public class EmployeeWageBuilder {
+	public static final int IS_PRESENT=1;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To EmployeeWage Programms");
+		empPresentOrAbsent();	
+	}
+	
+	public static void empPresentOrAbsent() {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck==IS_PRESENT) { 
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
 	
