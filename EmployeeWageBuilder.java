6a9649e537764d71b0f738aee3c339941ec63101
AkshataHasabi <akshuh818@gmail.com>
2022-01-03 21:51:05 +0530

    ADDED:calculated employee month wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,8 +4,10 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
-	int empHrs=0;
-	int empWage=0;
+	 public static int NUM_OF_WORKING_DAYS = 20; 
+	    int empHrs = 0;
+	    int empWage = 0;
+	    int totalEmpWage=0;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To EmployeeWage Programms");
@@ -14,6 +16,7 @@ public class EmployeeWageBuilder {
 		empWage.empDailyWages();
 		empWage.empPartTimeWage();
 		empWage.empPartTimeSwitchCase();
+		empWage.empMonthWage();
 	}
 	
 	public static void empPresentOrAbsent() {
@@ -65,6 +68,26 @@ public class EmployeeWageBuilder {
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			System.out.println("Emp Wage: " + empWage);   	
 	}
+	 
+	 public void empMonthWage() {
+	    	for (int day = 0; day <  NUM_OF_WORKING_DAYS; day++) {
+	    		int empCheck = (int)Math.floor(Math.random()*10)%3;
+	    		switch (empCheck) {
+	    			case IS_FULL_TIME:
+	    					empHrs = 8;
+	    					break;
+	    			case IS_PART_TIME:
+	    					empHrs = 4;
+	    					break;
+	    			default:
+	    					empHrs = 0;
+	    		}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: " + empWage);  
+	    	}
+	    	System.out.println("Total Emp Wage: " + totalEmpWage);
+	    }
 }
 	
     	
